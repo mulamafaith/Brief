9dea8f5450c7c1b86de7673bffca412b68c18535
mulamafaith <161311395+mulamafaith@users.noreply.github.com>
2024-07-09 20:35:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const learnMoreBtn = document.getElementById('learn-more-btn');
+const applyNowBtn = document.getElementById('apply-now-btn');
+
+learnMoreBtn.addEventListener('click', () => {
+  alert('Learn more about our programs!');
+});
+
+applyNowBtn.addEventListener('click', () => {
+  alert('Apply now and take the first step towards your animation career!');
+});
